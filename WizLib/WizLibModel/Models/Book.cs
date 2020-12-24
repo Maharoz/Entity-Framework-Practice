f98ae8dc340f8e759f073f4b25db5c7bbe0cdaf1
Maharoz <mahruj66@gmail.com>
2020-12-24 11:04:30 +0600

    Added more models on database

--- WizLib/WizLibModel/Models/Book.cs
+++ WizLib/WizLibModel/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace WizLib_Model.Models
@@ -9,12 +10,17 @@ namespace WizLib_Model.Models
     {
         [Key]
         public int Book_Id { get; set; }
+
         [Required]
         public string Title { get; set; }
+
         [Required]
+        [MaxLength(15)]
         public string ISBN { get; set; }
+
         [Required]
         public double Price { get; set; }
+
 
     }
 }
--- WizLib/WizLib_DataAccess/Data/ApplicationDbContext.cs
+++ WizLib/WizLib_DataAccess/Data/ApplicationDbContext.cs
@@ -16,5 +16,7 @@ namespace WizLib_DataAccess.Data
         public DbSet<Category> GetCategories { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
     }
 }
