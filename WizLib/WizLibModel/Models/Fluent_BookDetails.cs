2fa038ac3c7f79376d184cf0da6d81dca40c59d9
Maharoz <mahruj66@gmail.com>
2020-12-24 20:19:33 +0600

    Models added for fluentAPI

---
+++ WizLib/WizLibModel/Models/Fluent_BookDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WizLib_Model.Models
+{
+   public class Fluent_BookDetails
+    {
+        public int BookDetail_Id { get; set; }
+        public string NumberOfChapters{ get;set;}
+        public string NumberOfPages { get; set; }
+        public double Weight { get; set; }
+        public Book Book { get; set; }
+    }
+}
